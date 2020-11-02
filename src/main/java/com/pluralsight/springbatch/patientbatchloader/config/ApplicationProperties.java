2cfed9632eebaf2073fd6e562caebb74fe4a0659
nattapho.srisa <nattaphol.srisa@accenture.com>
2020-11-02 18:15:20 +0700

    Config: Link inputPath to application configuration

--- src/main/java/com/pluralsight/springbatch/patientbatchloader/config/ApplicationProperties.java
+++ src/main/java/com/pluralsight/springbatch/patientbatchloader/config/ApplicationProperties.java
@@ -9,6 +9,24 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
+    private final Batch batch = new Batch();
 
+    public Batch getBatch() {
+        return this.batch;
+    }
+
+    public static class Batch {
+
+        // Automatically linked to the application.batch.inputPath via @ConfigurationProperties
+        private String inputPath = "C:/Batch Study/patient-batch-loader/data";
+
+        public String getInputPath() {
+            return this.inputPath;
+        }
+
+        public void setInputPath(String inputPath) {
+            this.inputPath = inputPath;
+        }
+    }
 
 }
